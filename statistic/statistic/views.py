70e5229b6f32e97592664e3d26bd0a968cdb895f
HaqqiLucky <haqqiarifin@outlook.com>
2025-01-31 05:30:32 +0700

    up semua baru ada tanggal waktu

---
+++ statistic/statistic/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def homepage(request) :
+    now = datetime.now()
+    now_format = now.strftime("%Y-%m-%d")
+    day_format = now.strftime("%A")
+    time_format = now.strftime("%H:%M:%S")
+
+    tanggal = {
+        "date" : now_format,
+        "day" : day_format,
+        "time": time_format
+    }
+
+    return render(request, 'home.html', tanggal )
